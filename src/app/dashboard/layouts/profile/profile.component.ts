5fe0b9b6d27affbb451b69e5faed37af4fe1c085
robertomontoya01 <123822987+robertomontoya01@users.noreply.github.com>
2023-11-14 22:40:34 -0600

    Historial

--- src/app/dashboard/layouts/profile/profile.component.ts
+++ src/app/dashboard/layouts/profile/profile.component.ts
@@ -7,8 +7,43 @@ import { Router } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent {
 
+  TournamentsRecord = [
+    {
+      title: 'Tournament Record 1',
+      imageUrl: 'path/to/tournament1-image.jpg',
+      date: '2023-11-10',
+      game: 'Your Game',
+      info: 'Your info',
+      data: 'Your data',
+    },
+    {
+      title: 'Tournament Record 2',
+      imageUrl: 'path/to/tournament1-image.jpg',
+      date: '2023-11-10',
+      game: 'Your Game',
+      info: 'Your info',
+      data: 'Your data',
+    },
+    {
+      title: 'Tournament Record 3',
+      imageUrl: 'path/to/tournament1-image.jpg',
+      date: '2023-11-10',
+      game: 'Your Game',
+      info: 'Your info',
+      data: 'Your data',
+    },
+    {
+      title: 'Tournament Record 4',
+      imageUrl: 'path/to/tournament1-image.jpg',
+      date: '2023-11-10',
+      game: 'Your Game',
+      info: 'Your info',
+      data: 'Your data',
+    },
 
+  ]
 
 }
